530212dde0bdd47462c45e1e51da603015c724b1
Ilya Lozovskiy <mmeeaann30@gmail.com>
2025-03-22 14:02:06 +0300

    Implement modules and Spring Jdbc

--- trainer_ui_console/src/main/java/ru/spbu/frogsoill/Application.java
+++ trainer_ui_console/src/main/java/ru/spbu/frogsoill/Application.java
@@ -3,7 +3,7 @@ package ru.spbu.frogsoill;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.spbu.frogsoill.config.SpringConfig;
-import ru.spbu.frogsoill.controller.ConsoleController;
+import ru.spbu.frogsoill.console.controller.ConsoleController;
 
 public class Application {
     public static void main(String[] args) {
